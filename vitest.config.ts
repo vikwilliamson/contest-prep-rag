e67e2b3015c5f31dac3aeebc566472a6dd801ae2
Vik Williamson <29116979+vikwilliamson@users.noreply.github.com>
2026-05-07 10:06:44 -0700

    Update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./test/setup.ts'],
+    include: ['test/**/*.test.{ts,tsx}'],
   },
   resolve: {
     alias: {
